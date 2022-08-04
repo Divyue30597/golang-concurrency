1157c8e1e8e744eeba557143f7c6bad596af920e
Divyue30597 <tomchi.sharma305@outlook.com>
2022-08-04 21:46:42 +0530

    added the function for querying from cache and db

--- main.go
+++ main.go
@@ -1,5 +1,78 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"math/rand"
+	"time"
+)
 
+// cache is simple for this app and store the books in in-memory cache
+var cache = map[int]Book{}
+var randNum = rand.New(rand.NewSource(time.Now().UnixNano()))
+
+func main() {
+	/*
+		Scenario : If the cache has the data, the data is returned from the cache
+		If the database has the data, then the data is returned from the db and
+		stored in the cache if it is queried for the next time.
+	*/
+
+	for i := 0; i < 10; i++ {
+		id := randNum.Intn(10) + 1
+		// We are creating multiple go routine ->
+		// one for cache the other for database
+		// meaning we are making the code to work concurrently but not
+		// parallely.
+		// With this the output is printed.
+		go func(id int) {
+			if b, ok := queryCache(id); ok {
+				fmt.Println("from cache")
+				fmt.Println(b)
+			}
+		}(id)
+
+		go func(id int) {
+			if b, ok := queryDatabase(id); ok {
+				fmt.Println("from database")
+				fmt.Println(b)
+			}
+		}(id)
+
+		// fmt.Printf("Book not found with id: '%v'", id)
+
+		// What happens if there is no pause in the main function?
+		// We expect the data to be seen from the database query and this is
+		// considered to be the side effect to be pausing the main go routine which
+		// is bad. Now if we run again we see no output.
+
+		// Why we see no output? -> We see no output because the main function does
+		// not have anything to pause itself. So even thought it is generating
+		// those go routines, those go routines does not have enough time for those
+		// routines to complete themselves / to return. So the go programs works in
+		// such a way that we will generate all these 20 routines and the exit the
+		// program since there is nothing to execute.
+
+		// So as long as we try to pause our main program with time.Sleep we will
+		// be able to see the output and give the time for our go routines to complete.
+
+		time.Sleep(150 * time.Millisecond)
+	}
+	time.Sleep(2 * time.Second)
+}
+
+func queryCache(id int) (Book, bool) {
+	b, ok := cache[id] 
+	return b, ok
+}
+
+func queryDatabase(id int) (Book, bool) {
+	time.Sleep(100 * time.Millisecond)
+	for _, b := range books {
+		if b.ID == id {
+			cache[id] = b
+			return b, true
+		}
+	}
+
+	return Book{}, false
 }
